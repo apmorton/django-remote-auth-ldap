c2a2b7e1b2d85642936f130035b5391cbb4bb7af
Austin Morton <amorton@eaglabs.com>
2013-05-06 15:31:11 -0400

    fix incorrect name in middleware

--- src/django_remote_auth_ldap/middleware.py
+++ src/django_remote_auth_ldap/middleware.py
@@ -4,5 +4,5 @@ from django_remote_auth_ldap.conf import settings
 
 class RemoteUserMiddleware(middleware.RemoteUserMiddleware):
     @property
-    def HEADER(self):
+    def header(self):
         return settings.HEADER
