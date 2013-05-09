d622529f71db48542f8cea96a6ebe07bbeba2204
Austin Morton <amorton@eaglabs.com>
2013-05-09 17:26:35 -0400

    Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 
 
-version = '0.1.1'
+version = '0.1.2'
 
 install_requires = [
     'django-auth-ldap',
